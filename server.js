b0424c274c80448bfed51f6004521b4b78ae353d
ZilArt <ZilArt@MacBook-Air.local>
2025-07-28 00:52:01 +0300

    Product route updated

--- server.js
+++ server.js
@@ -97,22 +97,6 @@ app.use((req, res, next) => {
 });
 
 
-
-
-// ✅ File uploads
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // or wherever your upload folder is
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    const extension = file.originalname.split('.').pop();
-    cb(null, `${file.fieldname}-${uniqueSuffix}.${extension}`);
-  }
-});
-const upload = multer({ storage });
-
-
 const settingsRoutes = require("./routes/settings");
 app.use("/api/settings", settingsRoutes);
 
