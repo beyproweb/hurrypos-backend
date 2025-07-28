c5d36e621dd898190d54e445e8b00b57bb7c5ae1
ZilArt <ZilArt@MacBook-Air.local>
2025-07-28 15:19:20 +0300

    add new product

--- routes/upload.js
+++ routes/upload.js
@@ -3,7 +3,7 @@ const express = require("express");
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config({
+console.log("🌩️ Uploading to Cloudinary:", {
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
