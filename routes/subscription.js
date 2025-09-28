a5fd19baf574c41225fadb928be1093a004082dc
ZilArt <ZilArt@MacBook-Air.local>
2025-09-28 16:48:56 +0300

    deduct ingredients from stock

--- routes/subscription.js
+++ routes/subscription.js
@@ -74,6 +74,8 @@ router.post("/register", async (req, res) => {
 
 // ----------------- LOGIN -----------------
 router.post("/login", async (req, res) => {
+console.log("🟢 /api/login hit:", req.body);
+
   const { email, password } = req.body;
   try {
     const userRes = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
