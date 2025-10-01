1bf7c45a5ff7e8ffedf5facd4becb3109b3d1063
ZilArt <ZilArt@MacBook-Air.local>
2025-10-01 17:00:37 +0300

    Deploy backtend

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const pool = require("../db");
+// routes/products.js
+const { pool } = require("../db");   // ✅ destructure pool
+
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.use(authMiddleware);
